b4afd6b1a0bdbae2bae497384a0726d7a73ed416
“Arne <arne.zelasko@cubic.com>
2020-10-06 17:16:29 +0200

    fix test for non-ci environment

--- src/test/java/arnzel/mockMvcTestGenerator/util/ClassUtilsTest.java
+++ src/test/java/arnzel/mockMvcTestGenerator/util/ClassUtilsTest.java
@@ -1,9 +1,12 @@
 package arnzel.mockMvcTestGenerator.util;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.FileSystem;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,12 +16,17 @@ public class ClassUtilsTest {
     @Test
     public void testGetClassFromFile() throws Exception {
         // Setup
-        final File file = new File("src\\test\\java\\arnzel\\mockMvcTestGenerator\\util\\ClassUtilsTest.java");
+
+        final File file = getFile(ClassUtilsTest.class);
 
         // Run the test
         final Class result = ClassUtils.getClassFromFile(file);
 
         // Verify the results
         assertThat(result).isEqualTo(ClassUtilsTest.class);
+    }
+
+    private File getFile(Class clazz){
+        return new File("/Users/211897/IdeaProjects/mockMvcTestCreator/src/test/java/arnzel/mockMvcTestGenerator/util/ClassUtilsTest.java");
     }
 }
